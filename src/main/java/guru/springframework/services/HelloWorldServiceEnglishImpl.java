7c7e16499aacd24b72cbd2b42eaa4c6c24e2a263
Kathy <jackathy@htva.net>
2020-03-28 09:34:04 -0400

    Use java configuration class - pretend I don't have access to the actual java classes (English & Spanish implementations in this case), so annotations come out of the service classes & beans get created in the config class instead.

--- src/main/java/guru/springframework/services/HelloWorldServiceEnglishImpl.java
+++ src/main/java/guru/springframework/services/HelloWorldServiceEnglishImpl.java
@@ -1,13 +1,5 @@
 package guru.springframework.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
-/**
- * Created by jt on 10/26/15.
- */
-@Component
-@Profile({"default", "english"})
 public class HelloWorldServiceEnglishImpl implements HelloWorldService{
     @Override
     public String getGreeting() {
