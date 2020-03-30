0f7eff3069fb90dee4025a6ae7b283a6b12c70c9
Kathy <jackathy@htva.net>
2020-03-29 20:36:41 -0400

    All xml - no Java annotations - end of lesson 25

--- src/main/java/guru/springframework/controllers/GreetingController.java
+++ src/main/java/guru/springframework/controllers/GreetingController.java
@@ -1,59 +1,46 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.HelloWorldService;
-import guru.springframework.services.HelloWorldServiceFrenchImpl;
 import guru.springframework.services.HelloWorldServiceGermanImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 
-/**
- * Created by jt on 10/26/15.
- */
-//@Controller
 public class GreetingController {
 
     private HelloWorldService helloWorldService;
     private HelloWorldService helloWorldServiceGerman;
     private HelloWorldService helloWorldServiceFrench;
     private HelloWorldService helloWorldServicePolish;
+    private HelloWorldService helloWorldServiceRussian;
 
-    // This will pick up the bean by type from the profile and/or @Primary annotation
-    @Autowired
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
-    // We can put the implemented type as a parameter - not an excellent choice...
-    @Autowired
     public void setHelloWorldServiceGerman(HelloWorldServiceGermanImpl helloWorldServiceGerman){
         this.helloWorldServiceGerman = helloWorldServiceGerman;
     }
 
-    // Back to the interface, but use the default name (same as method name in config) to select the correct bean
-    @Autowired
-    @Qualifier("helloWorldServiceFrench")
     public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench){
         this.helloWorldServiceFrench = helloWorldServiceFrench;
     }
 
-    // This one picks up the "name" from the @Bean in the configuration class
-    @Autowired
-    @Qualifier("polish")
     public void setHelloWorldServicePolish(HelloWorldService helloWorldServicePolish){
         this.helloWorldServicePolish = helloWorldServicePolish;
+    }
+
+    public void setHelloWorldServiceRussian(HelloWorldService helloWorldServiceRussian){
+        this.helloWorldServiceRussian = helloWorldServiceRussian;
     }
 
     public String sayHello(){
 
         String greeting = helloWorldService.getGreeting();
-
         System.out.println(greeting);
+
         System.out.println(helloWorldServiceGerman.getGreeting());
         System.out.println(helloWorldServiceFrench.getGreeting());
         System.out.println(helloWorldServicePolish.getGreeting());
+        System.out.println(helloWorldServiceRussian.getGreeting());
 
         return greeting;
     }
-
 }
--- src/main/java/springboot/DependencyInjectionApplication.java
+++ src/main/java/springboot/DependencyInjectionApplication.java
@@ -2,14 +2,9 @@ package springboot;
 
 import guru.springframework.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
-//@ComponentScan("guru.springframework")
-// Use the component scan set up in the xml instead of the above java annotation
 @ImportResource("classpath:/spring/spring-config.xml")
 public class DependencyInjectionApplication {
 
@@ -17,7 +12,6 @@ public class DependencyInjectionApplication {
         ApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
 
         GreetingController controller = (GreetingController) ctx.getBean("greetingController");
-
-        controller.sayHello();
+        System.out.println("\nGreeting base on profile: " + controller.sayHello()+"\n");
     }
 }
